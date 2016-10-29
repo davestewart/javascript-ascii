7d3891a66adf09816da0c4107f99ba8c89ca1c49
Dave Stewart <dave@davestewart.co.uk>
2016-10-29 15:59:35 +0100

    Updated tree with options and better filtering

--- src/tree.js
+++ src/tree.js
@@ -1,56 +1,114 @@
-var depth;
-var maxDepth;
-var str;
-var path;
-var names;
-var pipes;
-
-function isFunction(value) {
-  return typeof value === 'function';
-}
-function isValue(value) {
-  return typeof value !== 'object' && !isFunction(value);
-}
-function isObject(value) {
-  return typeof value === 'object' && value !== null && typeof value !== 'undefined';
+function isFunction (value) { return value && typeof value === 'function'; }
+function isValue (value) { return value && typeof value !== 'object' && typeof value !== 'undefined' && !isFunction(value); }
+function isObject (value) { return value && typeof value === 'object' && value !== null; }
+function isClass (value) { return value && typeof value === 'object' && value.constructor && Object.keys(value).length == 0; }
+function className (value) {
+  return Array.isArray(value)
+    ? '[object Array]'
+    : Object.prototype.toString.call(source)
 }
 
-function print(name, value) {
-  name = name + (isFunction(value) ? '()' : '');
-  value = isValue(value) ? ' : ' + JSON.stringify(value) : '';
-  str += pipes.join('') + ' +- ' + name + value + '\n';
+function print (name, value, recursion) {
+
+  var val;
+  if (recursion) {
+    val = ' : <recursion>';
+  }
+  else if(typeof value === 'undefined'){
+    val = ' : undefined';
+  }
+  else if (Array.isArray(value)) {
+    val = ' : ' + (depth < options.depth ? '' : '<array>');
+  }
+  else if (isFunction(value)) {
+    name = name + ' ()';
+    val = '';
+  }
+  else if (isClass(value)) {
+    val = ' : ' + String(value);
+  }
+  else if (isObject(value)) {
+    val = depth < options.depth
+      ? ''
+      : '<object>';
+    val = ' : ' + val;
+  }
+  else if (isValue(value)) {
+    val = ' : ' + JSON.stringify(value)
+  }
+  else
+  {
+    val = ' : ' + value;
+  }
+
+  output += pipes.join('') + ' +- ' + name + val + '\n';
+
 }
 
-function process(parent, isLast) {
-  if (depth < maxDepth) {
+function process (parent, isLast) {
+
+  if (depth < options.depth) {
+
     depth++;
-    var keys = Object.keys(parent);
-    keys.sort();
+    objects.push(parent);
     pipes.push(isLast !== false ? '    ' : ' |  ');
+
+    var keys = Object.keys(parent);
+    if (options.sort) {
+      keys.sort();
+    }
+
     for (var k = 0; k < keys.length; k++) {
+
       var key = keys[k];
       var value = parent[key];
-      print(key, value);
-      if (isObject(value)) {
+      var recursion = !options.recursion && objects.indexOf(value) > -1;
+
+      print(key, value, recursion);
+      if (!recursion && isObject(value)) {
         process(value, k === keys.length - 1);
       }
+
     }
+
+    objects.pop();
     pipes.pop();
     depth--;
+
   }
 }
 
-export default function (value, name) {
+var defaults = {
+  depth: 5,
+  sort: false,
+  prop: true,
+  func: false
+};
+
+var options,
+  depth,
+  output,
+  path,
+  objects,
+  names,
+  pipes;
+
+export default function (value, opts, name) {
 
   depth = 0;
-  maxDepth = 5;
-  str = '';
+  output = '';
   path = '';
+  objects = [];
   names = [];
   pipes = [];
 
-  print(name || String(value), value);
-  depth++;
+  options = Object.assign(defaults, opts);
+  options.depth = parseInt(options.depth);
+
+  output = ' +- ' + (opts.name || name || String(value)) + '\n';
+
   process(value);
-  return str;
+
+  return output;
+
 }
